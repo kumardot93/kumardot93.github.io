03c0192183fcba2df4809b0f7a3229a360f94757
aditya <kumar.93@iitj.ac.in>
2020-08-04 00:00:56 +0530

    production changes

---
+++ precache-manifest.2f54ec8dc0ea6c10153ea494dae104ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6868b7e6ec2d5a67ef79fb26e6eb4d64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d995a6cc7e90ebd3fc4",
+    "url": "/static/css/main.400a1951.chunk.css"
+  },
+  {
+    "revision": "0e7e7b53aaa983b37450",
+    "url": "/static/js/2.70fb8b4f.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.70fb8b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d995a6cc7e90ebd3fc4",
+    "url": "/static/js/main.69c1707a.chunk.js"
+  },
+  {
+    "revision": "aab2099d422c4f313100",
+    "url": "/static/js/runtime-main.54ec0ee2.js"
+  },
+  {
+    "revision": "c8baefbc9476d30138705fcd4445eea5",
+    "url": "/static/media/Article1.c8baefbc.md"
+  },
+  {
+    "revision": "4fe3ffd4c2664359e1f24d12eabcca9c",
+    "url": "/static/media/pichtml.4fe3ffd4.png"
+  }
+]);
